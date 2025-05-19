6fe21dcd467527f4559831971678704a574fe428
maaria010 <159135507+maaria010@users.noreply.github.com>
2025-05-19 14:22:42 +0000

    calculatempo

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 const botoes = document.querySelectorAll(".botao");
-for(let i = 0; i <botoes.length; i++){
-    botoes  [i].onclick = function(){
-        for (let j = 0; j <botoes.length; j++){
-            botoes [j].classList.remove("ativo");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++){
+    botoes[i].onclick = function(){
+        for (let j = 0; j < botoes.length; j++){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
-            botoes[i].classList.add("ativo");   
+            botoes[i].classList.add("ativo");
+            textos[i].classList.add("ativo");
     }
+}
+const contadores = document.querySelectorAll("contador");
+const tempoObjetivo1 = new Date("2025-10-05T00.00.00");
+
+
+contadores[0]. textContent = calculaTempo (tempoObjetivo1) - tempoAtual;
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date ()
+    let tempoFinal = tempoObjetivo - tempoAtual;
 }
