d49ce90815a52ff3a32bf5d653ddb57a15b123c5
maaria010 <159135507+maaria010@users.noreply.github.com>
2025-05-26 14:25:39 +0000

    cronometro

--- main.js
+++ main.js
@@ -13,11 +13,37 @@ for (let i = 0; i < botoes.length; i++){
 }
 const contadores = document.querySelectorAll("contador");
 const tempoObjetivo1 = new Date("2025-10-05T00.00.00");
+const tempoObjetivo2 = new Date("2025-12-05T00.00.00");
+const tempoObjetivo3 = new Date("2025-12-05T00.00.00");
+const tempoObjetivo4 = new Date("2026-01-05T00.00.00");
 
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-contadores[0]. textContent = calculaTempo (tempoObjetivo1) - tempoAtual;
+contadores[0]. textContent = calculaTempo (tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date ()
     let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(minutos/60);
+    let horas = Math.floor(horas/60);
+    let dias = Math.floor(horas/24);
+
+    segundo %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + " dias "+ horas + " horas "+ minutos + " minutos "+ segundos + " segundos";
 }
+
+function atualizarCronometro(){
+    for(let i = 0 < contadores.length; i++){
+        contadores [i].textContent = calculaTempo(Tempo[i]);
+    }
+}
+function comecaCronometro(){
+   atualizarCronometro()
+   setInterval(atualizarCronometro, 1000);
+
+}
+comecaCronometro();
