c2079d1102c1ca2e90d976497db57418a9d9e37a
maaria010 <159135507+maaria010@users.noreply.github.com>
2025-06-09 13:54:32 +0000

    correção final

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const tempoObjetivo4 = new Date("2026-01-05T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+//contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
@@ -37,14 +37,18 @@ function calculaTempo(tempoObjetivo){
 
     if (tempoFinal > 0){
     return [dias, horas, minutos, segundos];
-}else{
-    return [0, 0, 0, 0,];
+    }else{
+        return[0, 0, 0, 0];
+    }
 }
 
 function atualizarCronometro(){
-    for(let i = 0; i < contadores.length; i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);
-    } 
+   for(let i = 0; i < contadores.length; i++){
+        document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
+   } 
 }
 function comecaCronometro(){
     atualizarCronometro();
